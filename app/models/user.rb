63bb7e30c64ed3f1f2babd6aec72977559d758bb
Mario Marroquin <mariomarroquin@marios-mbp.attlocal.net>
2019-02-06 01:29:35 -0800

    Add validation to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :posts
+
+  validates :username, presence: true
+  validates :password, presence: true, length: { minimum: 6 }, on: :create
 end
