c88ac9178d3e722ab80770e2f5ba450a151521a2
Mario Marroquin <mariomarroquin@marios-mbp.attlocal.net>
2019-02-06 12:57:25 -0800

    Add uniqness validation to username

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   has_secure_password
   has_many :posts
 
-  validates :username, presence: true
+  validates :username, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }, on: :create
 end
